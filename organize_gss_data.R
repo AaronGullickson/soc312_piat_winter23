f4907d6ceaa8be57f5c6b0052454ce42ba6920e5
Aaron Gullickson <aarongullickson@gmail.com>
2023-02-06 18:47:54 -0800

    Create skeleton of GSS data cleaning
    For the project focusing on the relationship between income and happiness

---
+++ organize_gss_data.R
@@ -0,0 +1,20 @@
+# This script will read in a GSS extract for the PIAT project focusing on
+# the relationship between income and happiness
+
+
+# Load libraries ----------------------------------------------------------
+
+library(here)
+library(tidyverse)
+
+
+# Read GSS data -----------------------------------------------------------
+
+
+
+# Recode/Clean ------------------------------------------------------------
+
+
+# Save data ---------------------------------------------------------------
+
+
